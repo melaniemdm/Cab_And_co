3fc1bc5be3f98a9190880a7fd17337b3d2a87b79
melaniemdm <melanieborja.pro@gmail.com>
2021-09-03 15:35:27 +0200

    feature(html): squelette et modal contact feature(css): header, logo, form feature(js) modal contact

---
+++ js/fichier.js
@@ -0,0 +1,7 @@
+var nodeForm = document.querySelector("#form")
+nodeForm.addEventListener("click", disappears)
+
+function disappears(){
+    var disappearsForm = document.querySelector("#form")
+    disappearsForm.style.visibility="hidden"
+}
