b538cbc3854c6085e3b20f848518b1d3ba0baefc
melaniemdm <melanieborja.pro@gmail.com>
2021-09-16 16:21:44 +0200

    fix(css) placement form fix(html) id nousContacter feature(js): appeard & keycode

--- js/fichier.js
+++ js/fichier.js
@@ -1,7 +1,19 @@
-var nodeForm = document.querySelector("#form")
+var nodeForm = document.querySelector("#send")
 nodeForm.addEventListener("click", disappears)
 
 function disappears(){
     var disappearsForm = document.querySelector("#form")
     disappearsForm.style.visibility="hidden"
 }
+
+var nodeForm = document.querySelector("#nousContacter")
+nodeForm.addEventListener("click", appears)
+function appears(){
+    var appearsForm = document.querySelector("#form")
+    appearsForm.style.visibility="visible"
+}
+document.addEventListener("keyup", function (e) {       
+    if (e.keyCode === 27) {
+        disappears()
+     
+    }});
